573a4acd6118ce97cb5b71915278df4db41804ae
Blake4171 <55347284+Blake4171@users.noreply.github.com>
2025-08-15 15:24:18 +0100

    Update GetPublicIP.py

--- GetPublicIP.py
+++ GetPublicIP.py
@@ -1,11 +1,14 @@
 import yaml
 import requests
+import json
+import os
 from datetime import datetime
 
 def public_ip():
-#load the contents of yaml folder created and stored in same directory. Store as the f variable
+
+    #load the contents of yaml folder created and stored in same directory. Store as the f variable
     with open('ddns.yaml') as f:
-     config = yaml.safe_load(f)
+        config = yaml.safe_load(f)
 
 # ip_public is what the url is stored under in the yaml file. This gets that url and stores it as the ip_url variable
     ip_url = config.get("ip_public")
@@ -13,13 +16,35 @@ def public_ip():
 # using the url, make a request to ipify 
     response = requests.get(ip_url)
     ip_address = response.text.strip() # response.text gets the body of the http request .strip removes any trailing whitespace
-
-# store the current date & time as the variable timestamp
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S") #.strftime details what format 'timestamp' should be outputted as 
-
-    #print contents of 'ip_address' and 'timestamp' variables 
-    print("The public ip is", ip_address,'at',timestamp)
-
+    return ip_address
 public_ip()
 
-# Next, use the above function and cloudflare api to change the dns record on my account.
+#def current_ip():
+    # store the current date & time as the variable timestamp
+    #timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S") #.strftime details what format 'timestamp' should be outputted as 
+
+    #print contents of 'ip_address' and 'timestamp' variables 
+    #print("The public ip is", public_ip(),'at',timestamp)
+    
+ #   print(public_ip())
+
+  #  current_ip()
+
+def check_ip(): 
+    with open("IPcheck.json", "r") as file: #opens and reads json file 'IPcheck.json
+       last_ip = json.load(file) #stores contents as 'last_ip'
+
+    if last_ip == public_ip:  
+        print('There has been no recent change in your networks public IP')
+    else:
+        with open('IPcheck.json', 'w')as file: #if ip store in json 'last_ip' is not the same as the current IP. Store the new IP in the json file
+            json.dump(public_ip(), file)
+
+    
+    
+check_ip()   
+
+
+
+ 
+    
