3430fe9dd92882fa0214b751637a4bddd4a99fe7
Blake4171 <55347284+Blake4171@users.noreply.github.com>
2025-08-21 09:06:57 +0100

    Update GetPublicIP.py

--- GetPublicIP.py
+++ GetPublicIP.py
@@ -4,7 +4,8 @@ import json
 import os
 from datetime import datetime
 
-def public_ip():
+
+def current_ip():
 
     #load the contents of yaml folder created and stored in same directory. Store as the f variable
     with open('ddns.yaml') as f:
@@ -17,32 +18,33 @@ def public_ip():
     response = requests.get(ip_url)
     ip_address = response.text.strip() # response.text gets the body of the http request .strip removes any trailing whitespace
     return ip_address
-public_ip()
+current_ip()
 
-#def current_ip():
-    # store the current date & time as the variable timestamp
-    #timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S") #.strftime details what format 'timestamp' should be outputted as 
-
-    #print contents of 'ip_address' and 'timestamp' variables 
-    #print("The public ip is", public_ip(),'at',timestamp)
-    
- #   print(public_ip())
-
-  #  current_ip()
-
-def check_ip(): 
+def ip_2json(): 
     with open("IPcheck.json", "r") as file: #opens and reads json file 'IPcheck.json
        last_ip = json.load(file) #stores contents as 'last_ip'
 
-    if last_ip == public_ip:  
-        print('There has been no recent change in your networks public IP')
-    else:
+    if last_ip != current_ip: 
         with open('IPcheck.json', 'w')as file: #if ip store in json 'last_ip' is not the same as the current IP. Store the new IP in the json file
-            json.dump(public_ip(), file)
+            json.dump(current_ip(), file)
+            return(last_ip, current_ip)
+        
+ip_2json()   
 
-    
-    
-check_ip()   
+def update_dns():
+    if ip_2json != current_ip:
+        client.dns.records.update(
+            zone_id=zone_id,
+            dns_record_id=record_id,
+            type="A",
+            name="",
+            content=current_ip(),
+            ttl=300,
+            proxied=True
+            )
+
+update_dns()
+
 
 
 
